540e3007aae6b1f4ba691251059fbc446d67c759
Sergey Dmitriev <sergey@terapool.pro>
2019-01-04 18:18:11 +0300

    First commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+
+const mongoose = require("mongoose");
+
+require("dotenv").config({ path: "variables.env" });
+
+const Recipe = require("./models/Recipe");
+
+const User = require("./models/User");
+
+// Connects to DB
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("DB connected"))
+  .catch(err => console.error(err));
+
+// Initializes application
+
+const app = express();
+
+const PORT = process.env.PORT || 4444;
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
